a7787aefb925d59b7f8db83c968d92c3346f0a82
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-10-25 18:10:01 +0900

    [chore] gatsby-config 파일 코드 수정
    - siteUrl 수정.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,7 @@ dotenv.config({
 export const config: GatsbyConfig = {
   siteMetadata: {
     title: `Yongwoo JJ Blog`,
-    siteUrl: `https://jjisyongwoo.netlify.app/`,
+    siteUrl: `https://blog.yongwoo.me`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
