254ea2e4d876d1e88a5190244ecd27f3ff2b7474
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-11-06 21:36:43 +0900

    [chore] mdx 파일로부터 import 해올 때 타입스크립트로 인해 생기는 오류 방지
    - global.d.ts에 모듈 타입 선언.

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,8 @@ declare module "*.scss" {
   const content: { [className: string]: string };
   export = content;
 }
+
+declare module "*.mdx" {
+  let MDXComponent: (props) => JSX.Element;
+  export default MDXComponent;
+}
