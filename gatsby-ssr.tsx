dc4596422557f597483448e64e18139c7bfa824c
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-11-09 02:20:51 +0900

    [perf] pretendard 글꼴 preload 기능 추가
    - 첫 페이지 로딩 시 한글을 사용하는데, Leon Sans 글꼴이 한글을 지원하지 않아서 Pretendard가 사용되므로 preload에 추가.

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -4,16 +4,23 @@ export const onRenderBody = ({ setHeadComponents }: any) => {
   setHeadComponents([
     <link
       rel="preload"
-      href="./static/fonts/leonsans/leon-sans-fonts/LeonSans-Medium.woff2"
+      href="./static/fonts/leonsans/leon-sans-fonts/LeonSans-Light.woff2"
       as="font"
       type="font/woff2"
       crossOrigin="anonymous"
     />,
     <link
       rel="preload"
-      href="./static/fonts/kopubworld/KoPubWorld Batang_Pro Medium_Subset.woff2"
+      href="./static/fonts/kopubworld/KoPubWorld-Batang-Pro-Light-Subset.woff2"
       as="font"
       type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      rel="preload"
+      href="./static/fonts/Pretendard/files/Pretendard-ExtraLight-Subset.otf"
+      as="font"
+      type="font/opentype"
       crossOrigin="anonymous"
     />,
   ]);
