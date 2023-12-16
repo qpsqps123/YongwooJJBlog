562172a4fd2d312a8335ff5d40ae60e7d92d4a1d
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-16 18:46:59 +0900

    [chore] robots plugin 설치, local search & useflexsearch 삭제
    - gatsby-plugin-robots-txt 설치
    - gatsby-plugin-local-search 삭제
    - react-use-flexsearch 삭제

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,8 @@ dotenv.config({
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `YongwooJJ Blog`,
-    siteUrl: `https://blog.yongwoo.me`,
+    siteUrl: `https://blog.yongwoo.me/`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   flags: {
     DEV_SSR: true,
@@ -26,6 +23,15 @@ const config: GatsbyConfig = {
         display: "standalone",
         icon: "./src/images/icon/favicon.png",
         crossOrigin: `use-credentials`,
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://blog.yongwoo.me/",
+        sitemap: "https://blog.yongwoo.me/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     {
@@ -81,77 +87,9 @@ const config: GatsbyConfig = {
         ],
       },
     },
-    {
-      resolve: "gatsby-plugin-local-search",
-      options: {
-        // A unique name for the search index. This should be descriptive of
-        // what the index contains. This is required.
-        name: "pages",
-
-        // Set the search engine to create the index. This is required.
-        // The following engines are supported: flexsearch, lunr
-        engine: "flexsearch",
-
-        // Provide options to the engine. This is optional and only recommended
-        // for advanced users.
-        //
-        // Note: Only the flexsearch engine supports options.
-        engineOptions: {
-          tokenize: "forward",
-        },
-
-        // GraphQL query used to fetch all data for the search index. This is
-        // required.
-        query: `
-          {
-            allMdx {
-              nodes {
-                id
-                frontmatter {
-                  title
-                  slug
-                  post
-                }
-                body
-                excerpt(pruneLength: 195)
-              }
-            }
-          }
-        `,
-
-        // Field used as the reference value for each document.
-        // Default: 'id'.
-        ref: "id",
-
-        // List of keys to index. The values of the keys are taken from the
-        // normalizer function below.
-        // Default: all fields
-        index: ["title", "body"],
-
-        // List of keys to store and make available in your UI. The values of
-        // the keys are taken from the normalizer function below.
-        // Default: all fields
-        // store: ["id", "path", "title"],
-
-        // Function used to map the result from the GraphQL query. This should
-        // return an array of items to index in the form of flat objects
-        // containing properties to index. The objects must contain the `ref`
-        // field above (default: 'id'). This is required.
-        normalizer: ({ data }: any) =>
-          data.allMdx.nodes.map((node: any) => ({
-            id: node.id,
-            title: node.frontmatter.title,
-            slug: node.frontmatter.slug,
-            post: node.frontmatter.post,
-            body: node.body,
-            excerpt: node.excerpt,
-          })),
-      },
-    },
     "gatsby-plugin-image",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sass",
-    "gatsby-plugin-sitemap",
     "gatsby-transformer-remark",
   ],
 };
