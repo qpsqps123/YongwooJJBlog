8912e96b06b2b3aedaf9aa5077f62f2838aa9028
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-30 17:57:52 +0900

    [fix] metadata의 description이 추가되지 않던 문제 해결
    - seo 컴포넌트에 description을 추가 적용해서 해결.

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -3,21 +3,30 @@ import { graphql, useStaticQuery } from "gatsby";
 
 interface PropType {
   title: string;
+  description?: string;
 }
-export const SEO = ({ title }: PropType) => {
+
+export const SEO = ({ title, description }: PropType) => {
   const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
           title
+          description
         }
       }
     }
   `);
 
+  const seo = {
+    title: `${title} | ${data.site.siteMetadata.title}`,
+    description: description || data.site.siteMetadata.description,
+  };
+
   return (
-    <title>
-      {title} | {data.site.siteMetadata.title}
-    </title>
+    <React.Fragment>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+    </React.Fragment>
   );
 };
