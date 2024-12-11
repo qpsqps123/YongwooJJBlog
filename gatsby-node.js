82ab8c287bbadc4cad2c7221f8ea0a63b84474d3
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-12-11 20:36:04 +0900

    bugfix: fix build error trial3

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src"),
+        "@": path.resolve(__dirname, "src/"),
       },
     },
   });
