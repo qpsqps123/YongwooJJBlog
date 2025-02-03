aa549cb116c86493aa1c91dcc28a73b3cd371953
qpsqps123 <qpsqps321@gmail.com>
2025-02-03 20:52:07 +0900

    change github link style on the projects page(text link below instead of github icon link)

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -17,18 +17,18 @@ const Project = ({ deployHrefSrc, deployLinkLabel, imgSrc, imgAlt, role, workTyp
         </li>
         {workType}
       </ul>
-      <div className={classes.headingContainer}>
-        <h2 className={classes.title}>
-          <a href={deployHrefSrc} target="_blank" rel="noopener noreferrer">{title}</a>
-        </h2>
-        <a href={githubHrefSrc} className={classes.githubLink} data-invert target="_blank" rel="noopener noreferrer" aria-label={githubLinkLabel} >
-          <StaticImage src="../../images/logo/logo-github-black.svg" alt="Github logo" width={20} height={20} />
-        </a>
-      </div>
+      <h2 className={classes.title}>
+        <a href={deployHrefSrc} target="_blank" rel="noopener noreferrer">{title}</a>
+      </h2>
       {description}
       <ul className={classes.techStackContainer} aria-label="사용 기술">
         {techStack}
       </ul>
+      <div>
+        <a href={githubHrefSrc} className={classes.githubLink} data-invert target="_blank" rel="noopener noreferrer" aria-label={githubLinkLabel} >
+          github
+        </a>
+      </div>
     </article>
   );
 };
